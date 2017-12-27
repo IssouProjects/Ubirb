e09c9d973a9e62a4d07b1a867a2147a53a2b5cac
Vincent Redouté <vincent.redoute@live.fr>
2017-12-27 15:00:59 +0100

    now signup at the start

--- app/src/main/java/com/issouprojects/ubirb/start/SignUp.java
+++ app/src/main/java/com/issouprojects/ubirb/start/SignUp.java
@@ -1,4 +1,4 @@
-package com.issouprojects.ubirb;
+package com.issouprojects.ubirb.start;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,11 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.issouprojects.ubirb.MainActivity;
+import com.issouprojects.ubirb.R;
+
 /**
  * Created by Vincent Redouté on 27/12/2017.
  */
 
-public class SignIn extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
     private Button start;
     private Intent intent;
@@ -19,14 +22,14 @@ public class SignIn extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_signup);
 
         start = (Button) findViewById(R.id.start);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(SignIn.this, MainActivity.class);
+                intent = new Intent(SignUp.this, MainActivity.class);
                 startActivity(intent);
             }
         });
