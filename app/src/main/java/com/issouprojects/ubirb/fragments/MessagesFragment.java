ee6e8e8513b86106c4c0d0a3bae096fd67a2968d
Vincent Redouté <vincent.redoute@live.fr>
2017-12-30 13:30:47 +0100

    looking like figma

--- app/src/main/java/com/issouprojects/ubirb/fragments/MessagesFragment.java
+++ app/src/main/java/com/issouprojects/ubirb/fragments/MessagesFragment.java
@@ -28,6 +28,23 @@ public class MessagesFragment extends Fragment {
     private TextView txtPierre;
     private ImageView imgPierre;
 
+    private TextView nomPaul;
+    private TextView txtPaul;
+    private ImageView imgPaul;
+
+    private TextView nomPaul2;
+    private TextView txtPaul2;
+    private ImageView imgPaul2;
+
+    private TextView nomJean;
+    private TextView txtJean;
+    private ImageView imgJean;
+
+    private TextView nomBosh;
+    private ImageView imgBosh;
+
+    private TextView nomXbox;
+    private ImageView imgXbox;
 
     public MessagesFragment() {
         // Required empty public constructor
@@ -56,15 +73,35 @@ public class MessagesFragment extends Fragment {
         txtPierre = rootView.findViewById(R.id.txtPierre);
         imgPierre = rootView.findViewById(R.id.imgPierre);
 
+        nomPaul = rootView.findViewById(R.id.nomPaul);
+        txtPaul = rootView.findViewById(R.id.txtPaul);
+        imgPaul = rootView.findViewById(R.id.imgPaul);
+
+        nomPaul2 = rootView.findViewById(R.id.nomPaul2);
+        txtPaul2 = rootView.findViewById(R.id.txtPaul2);
+        imgPaul2 = rootView.findViewById(R.id.imgPaul2);
+
+        nomJean = rootView.findViewById(R.id.nomJean);
+        txtJean = rootView.findViewById(R.id.txtJean);
+        imgJean = rootView.findViewById(R.id.imgJean);
+
+        nomBosh = rootView.findViewById(R.id.nomBosh);
+        imgBosh = rootView.findViewById(R.id.imgBosh);
+
+        nomXbox = rootView.findViewById(R.id.nomXbox);
+        imgXbox = rootView.findViewById(R.id.imgXbox);
+
         tous.setTextColor(Color.WHITE);
         offre.setTextColor(Color.LTGRAY);
         recois.setTextColor(Color.LTGRAY);
 
+        hideOffre(View.INVISIBLE);
+
         offre.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hideKevin(View.INVISIBLE);
-                hidePierre(View.INVISIBLE);
+                hideTous(View.INVISIBLE);
+                hideOffre(View.VISIBLE);
                 offre.setTextColor(Color.WHITE);
                 tous.setTextColor(Color.LTGRAY);
             }
@@ -73,8 +110,8 @@ public class MessagesFragment extends Fragment {
         tous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                hideKevin(View.VISIBLE);
-                hidePierre(View.VISIBLE);
+                hideTous(View.VISIBLE);
+                hideOffre(View.INVISIBLE);
                 tous.setTextColor(Color.WHITE);
                 offre.setTextColor(Color.LTGRAY);
             }
@@ -94,15 +131,33 @@ public class MessagesFragment extends Fragment {
         super.onDetach();
     }
 
-    private void hideKevin(int visibility){
+    private void hideTous(int visibility){
         nomKevin.setVisibility(visibility);
         txtKevin.setVisibility(visibility);
         imgKevin.setVisibility(visibility);
-    }
 
-    private void hidePierre(int visibility){
         nomPierre.setVisibility(visibility);
         txtPierre.setVisibility(visibility);
         imgPierre.setVisibility(visibility);
+
+        nomPaul.setVisibility(visibility);
+        txtPaul.setVisibility(visibility);
+        imgPaul.setVisibility(visibility);
+    }
+
+    private void hideOffre(int visibility){
+        nomPaul2.setVisibility(visibility);
+        txtPaul2.setVisibility(visibility);
+        imgPaul2.setVisibility(visibility);
+
+        nomJean.setVisibility(visibility);
+        txtJean.setVisibility(visibility);
+        imgJean.setVisibility(visibility);
+
+        nomBosh.setVisibility(visibility);
+        imgBosh.setVisibility(visibility);
+
+        nomXbox.setVisibility(visibility);
+        imgXbox.setVisibility(visibility);
     }
 }
