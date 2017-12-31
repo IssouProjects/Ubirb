7016eb919a5a5ee3528de1034434605c08fb5539
Vincent Redouté <vincent.redoute@live.fr>
2017-12-31 16:51:07 +0100

    can see details of a request

---
+++ app/src/main/java/com/issouprojects/ubirb/activities/RequestDetails.java
@@ -0,0 +1,34 @@
+package com.issouprojects.ubirb.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.issouprojects.ubirb.R;
+
+
+/**
+ * Created by Vincent Redouté on 31/12/2017.
+ */
+
+public class RequestDetails extends AppCompatActivity {
+
+    private ImageButton back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_requestdetails);
+
+        back = (ImageButton) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/issouprojects/ubirb/fragments/RequestsFragment.java
+++ app/src/main/java/com/issouprojects/ubirb/fragments/RequestsFragment.java
@@ -1,16 +1,23 @@
 package com.issouprojects.ubirb.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.issouprojects.ubirb.R;
+import com.issouprojects.ubirb.activities.RequestDetails;
+import com.issouprojects.ubirb.start.SignIn;
+import com.issouprojects.ubirb.start.SignUp;
 
 
 public class RequestsFragment extends Fragment {
+
+    private Button details;
 
     public RequestsFragment() {
         // Required empty public constructor
@@ -27,6 +34,15 @@ public class RequestsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_requests, container, false);
 
+        details = rootView.findViewById(R.id.details);
+
+        details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), RequestDetails.class);
+                startActivity(intent);
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootView;
