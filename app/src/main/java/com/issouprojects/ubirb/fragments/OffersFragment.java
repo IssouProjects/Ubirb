42b8b43766734937628242ba561206c4689b8f59
Vincent Redouté <vincent.redoute@live.fr>
2018-01-01 17:45:06 +0100

    offers details

--- app/src/main/java/com/issouprojects/ubirb/fragments/OffersFragment.java
+++ app/src/main/java/com/issouprojects/ubirb/fragments/OffersFragment.java
@@ -1,16 +1,23 @@
 package com.issouprojects.ubirb.fragments;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.issouprojects.ubirb.R;
+import com.issouprojects.ubirb.activities.OfferDetails;
 
 
 public class OffersFragment extends Fragment {
+
+    private Button details;
+    private ImageButton add;
 
     public OffersFragment() {
         // Required empty public constructor
@@ -27,6 +34,24 @@ public class OffersFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_offers, container, false);
 
+        details = rootView.findViewById(R.id.details);
+        add = rootView.findViewById(R.id.add);
+
+        details.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), OfferDetails.class);
+                startActivity(intent);
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getActivity(), RequestCreate.class);
+                //startActivity(intent);
+            }
+        });
 
         // Inflate the layout for this fragment
         return rootView;
