4995b94ba6d22617e39401571ce2da2dec1fc24b
Victor <vb4007@gmail.com>
2018-01-02 19:26:47 +0100

    Back button works

--- app/src/main/java/com/issouprojects/ubirb/RequestMessages.java
+++ app/src/main/java/com/issouprojects/ubirb/RequestMessages.java
@@ -2,12 +2,25 @@ package com.issouprojects.ubirb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RequestMessages extends AppCompatActivity {
+
+    private ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_messages);
+
+        back = (ImageButton) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
