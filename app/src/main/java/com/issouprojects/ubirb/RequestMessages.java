cb998abf3dfae07350688ba63cc987d0a96bdeaa
Victor <vb4007@gmail.com>
2018-01-02 18:18:27 +0100

    Create RequestMessages activity

---
+++ app/src/main/java/com/issouprojects/ubirb/RequestMessages.java
@@ -0,0 +1,13 @@
+package com.issouprojects.ubirb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestMessages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_messages);
+    }
+}
