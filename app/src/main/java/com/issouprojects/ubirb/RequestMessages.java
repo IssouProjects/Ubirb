f3dd82a5a0921b7d4fc18a145d703c6bdce25c3e
Victor <vb4007@gmail.com>
2018-01-03 14:44:02 +0100

    Accept button leads to the end transaction box

--- app/src/main/java/com/issouprojects/ubirb/RequestMessages.java
+++ app/src/main/java/com/issouprojects/ubirb/RequestMessages.java
@@ -1,13 +1,18 @@
 package com.issouprojects.ubirb;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class RequestMessages extends AppCompatActivity {
 
     private ImageButton back;
+    private Button accept;
+    private ConstraintLayout demandeDePret;
+    private ConstraintLayout finDePret;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +20,22 @@ public class RequestMessages extends AppCompatActivity {
         setContentView(R.layout.activity_request_messages);
 
         back = (ImageButton) findViewById(R.id.back);
+        accept = findViewById(R.id.acceptButton);
+        demandeDePret = findViewById(R.id.demandedepret);
+        finDePret = findViewById(R.id.findepret);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                demandeDePret.setVisibility(View.INVISIBLE);
+                finDePret.setVisibility(View.VISIBLE);
             }
         });
     }
