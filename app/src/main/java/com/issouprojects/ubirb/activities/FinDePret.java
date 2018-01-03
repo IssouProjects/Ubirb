d4c28aaa615a5d3b309b595c4fadcd3d313e5f0e
Victor <vb4007@gmail.com>
2018-01-03 15:19:37 +0100

    Back button works

--- app/src/main/java/com/issouprojects/ubirb/activities/FinDePret.java
+++ app/src/main/java/com/issouprojects/ubirb/activities/FinDePret.java
@@ -2,14 +2,27 @@ package com.issouprojects.ubirb.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.issouprojects.ubirb.R;
 
 public class FinDePret extends AppCompatActivity {
 
+    private ImageButton back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fin_de_pret);
+
+        back = (ImageButton) findViewById(R.id.back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
