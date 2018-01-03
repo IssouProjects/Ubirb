981e915819443fd3aeb9b98262a5e739c7528c4c
Victor <vb4007@gmail.com>
2018-01-03 14:46:28 +0100

    Create FinDePret activity

---
+++ app/src/main/java/com/issouprojects/ubirb/activities/FinDePret.java
@@ -0,0 +1,15 @@
+package com.issouprojects.ubirb.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.issouprojects.ubirb.R;
+
+public class FinDePret extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fin_de_pret);
+    }
+}
